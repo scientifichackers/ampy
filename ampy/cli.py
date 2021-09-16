a35b56c4b3e4162348cc97cb0dd801920dfb12ad
Michael Bentley <mikebentley15@gmail.com>
2021-09-16 12:10:15 -0600

    ampy get: allow destination to be a directory

--- ampy/cli.py
+++ ampy/cli.py
@@ -21,6 +21,7 @@
 # SOFTWARE.
 from __future__ import print_function
 import os
+import pathlib
 import platform
 import posixpath
 import re
@@ -101,8 +102,12 @@ def cli(port, baud, delay):
 
 @cli.command()
 @click.argument("remote_file")
-@click.argument("local_file", type=click.File("wb"), required=False)
-def get(remote_file, local_file):
+@click.argument(
+    "local_path",
+    type=click.Path(writable=True, allow_dash=True, path_type=pathlib.Path),
+    required=False
+)
+def get(remote_file, local_path):
     """
     Retrieve a file from the board.
 
@@ -110,8 +115,9 @@ def get(remote_file, local_file):
     locally.  You must pass at least one argument which is the path to the file
     to download from the board.  If you don't specify a second argument then
     the file contents will be printed to standard output.  However if you pass
-    a file name as the second argument then the contents of the downloaded file
-    will be saved to that file (overwriting anything inside it!).
+    a file name or a directory as the second argument then the contents of the
+    downloaded file will be saved to that file or directory respectively
+    (overwriting anything inside it!).
 
     For example to retrieve the boot.py and print it out run:
 
@@ -119,16 +125,24 @@ def get(remote_file, local_file):
 
     Or to get main.py and save it as main.py locally run:
 
-      ampy --port /board/serial/port get main.py main.py
+      ampy --port /board/serial/port get main.py ./
+
+    You can also specify "-" as the destination to explicitly specify to output
+    to standard output.
     """
     # Get the file contents.
     board_files = files.Files(_board)
     contents = board_files.get(remote_file)
     # Print the file out if no local file was provided, otherwise save it.
-    if local_file is None:
+    if local_path is None or str(local_path) == "-":
         print(contents.decode("utf-8"))
+    elif local_path.is_dir():
+        remote_path = pathlib.Path(remote_file)
+        with local_path.joinpath(remote_path.name).open(mode='wb') as local_file:
+            local_file.write(contents)
     else:
-        local_file.write(contents)
+        with local_path.open(mode='wb') as local_file:
+            local_file.write(contents)
 
 
 @cli.command()
